72d8cd2dc9d5de31a286ad6e220dd11c59c24b2a
Lauren Hecht <laurenhecht@robotlake.com>
2014-10-06 02:53:26 -0400

    remove old stuff

--- src/mongo/db/storage/wiredtiger/wiredtiger_init.cpp
+++ src/mongo/db/storage/wiredtiger/wiredtiger_init.cpp
@@ -34,7 +34,6 @@
 #include "mongo/db/global_environment_d.h"
 #include "mongo/db/global_environment_experiment.h"
 #include "mongo/db/storage/kv/kv_storage_engine.h"
-#include "mongo/db/storage/wiredtiger/wiredtiger_engine.h"
 #include "mongo/db/storage/wiredtiger/wiredtiger_kv_engine.h"
 #include "mongo/db/storage/wiredtiger/wiredtiger_global_options.h"
 #include "mongo/db/storage_options.h"
